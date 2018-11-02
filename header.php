4555ae374238c1df3e0d7ac562adb19aed47eef7
Rathor597 <ramandeep.rathor97@gmail.com>
2018-11-02 17:42:46 -0400

    fixed sql

--- header.php
+++ header.php
@@ -13,31 +13,25 @@ require("./includes/functions.php");
 require("./includes/constants.php");
 require("./includes/db.php");
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-   		"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<!--The nav bar and the layout of the site is based on a premade bootstrap template -->
-<!--https://startbootstrap.com/template-overviews/modern-business/-->
+<!DOCTYPE html>
+
   <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
   <!-- favicon -->
-  <link rel="icon" href="./images/favicon.png">
+  <link rel="icon" href="./images/favicon.png"/>
     <title>Homes for Gnomes</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
     <!-- Custom styles for this template -->
-    <link href="css/webd3201.css" rel="stylesheet">
+    <link href="css/webd3201.css" rel="stylesheet"/>
 
     <!--  JQuery import-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="vendor/jquery/jquery.min.js"></script>
+    <script type="text/javascript" src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   </head>
 
@@ -45,9 +39,9 @@ require("./includes/db.php");
     <!-- Navigation -->
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top" style="background-color:#982a4f;">
       <div class="container">
-          <img src="./images/HomesForGnomes.png" alt="logo" style="width:50px;">
+          <img src="./images/HomesForGnomes.png" alt="logo" style="width:50px;"/>
         <a class="navbar-brand" href="index.php">Homes for Gnomes</a>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-target="#navbarResponsive">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
@@ -82,16 +76,16 @@ require("./includes/db.php");
               {
                   echo "<li class=\"nav-item dropdown\">
                     <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdownPortfolio\"
-                    data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
+                    data-toggle=\"dropdown\">
                       Profile
                     </a>
-                    <div class=\"dropdown-menu dropdown-menu-right\" aria-labelledby=\"navbarDropdownPortfolio\">
+                    <div class=\"dropdown-menu dropdown-menu-right\">
                       <a class=\"dropdown-item\" href=\"change-password.php\">Change Password</a>
                       <a class=\"dropdown-item\" href=\"update.php\">Update</a>
                     </div>";
               }?>
             <!-- <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-expanded="false">
                 Profile
               </a>
               <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
@@ -100,10 +94,10 @@ require("./includes/db.php");
               </div>
             </li>-->
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown">
                 Listings
               </a>
-              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+              <div class="dropdown-menu dropdown-menu-right">
                 <a class="dropdown-item" href="listing-display.php">Display Listings</a>
                 <a class="dropdown-item" href="listing-search.php">Search Listings</a>
                 <?php if ($_SESSION['userType'] == ADMIN || $_SESSION['userType'] == AGENT)
--- sql/property_parking.sql
+++ sql/property_parking.sql
@@ -1,22 +1,22 @@
---  Ramandeep Rathor
+﻿--  Ramandeep Rathor
 --  Nathan Morris
 -- Date: 2018/10/29
 
 DROP TABLE IF EXISTS property_flooring;
 
-CREATE TABLE property_flooring(
+CREATE TABLE property_parking(
 value INT PRIMARY KEY,
 property VARCHAR(30) NOT NULL
 );
 
-ALTER TABLE property_flooring OWNER TO astillak;-- Author: Group 24
+ALTER TABLE property_parking OWNER TO group24_admin;-- Author: Group 24
 
 
-INSERT INTO property_flooring (value, property) VALUES (1, 'Single car port');
+INSERT INTO property_parking (value, property) VALUES (1, 'Single car port');
 
-INSERT INTO property_flooring (value, property) VALUES (2, 'Double car port');
+INSERT INTO property_parking (value, property) VALUES (2, 'Double car port');
 
-INSERT INTO property_flooring (value, property) VALUES (4, 'Reserved');
+INSERT INTO property_parking (value, property) VALUES (4, 'Reserved');
 
-INSERT INTO property_flooring (value, property) VALUES (8, 'lor systemt');
+INSERT INTO property_parking (value, property) VALUES (8, 'lor systemt');
 
