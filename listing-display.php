e7b1cad82d2907c7005a94909a8dda8377c3d08b
Musab Nazir <musabnazir@gmail.com>
2018-11-22 20:09:37 -0500

    Listing display and image map added (WIP)

--- listing-display.php
+++ listing-display.php
@@ -2,22 +2,62 @@
 /*
 Name:         	Ramandeep Rathor
 Name:           Musab Nizar
-Name:			      Kevin Astilla
-Name:			      Nathan Morris
-Description:  	Display Listings File For Homes For Gnomes
+Name:			Kevin Astilla
+Name:			Nathan Morris
+Description:  	Header File For Homes For Gnomes
 Date:         	28 September 2018
 */
-  $title = "WEBD2201 - Web Development - Fundamentals";
-  $date = "12 April 2018";
-  $file = "template.php";
-  $banner = "Lab 9: Database/PHP Lab - User Login";
-  $description = "This page will be a log in page for the website and a new functions.php file will contain some shared functions for use throughout the website.";
 
 require "header.php";
 ?>
   <!-- start of main page content -->
-
-
+<div class="container">
+  <div class="row" style="margin-top:75px">
+    <div class="col-4">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Listing Preview Headline</h5>
+                <img src="./images/Hobbiton-Matamata-SaraOrme-800x600.jpg" alt="" width="300px">
+                price<br/>
+                Beds<br/>
+                baths<br/>
+            </div>
+        </div>
+    </div>
+    <div class="col-4">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Listing Preview Headline</h5>
+                <img src="./images/hobbiton.jpg" alt="" width="300px">
+                <p>Stuff about the listing</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-4">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Listing Preview Headline</h5>
+                <img src="./images/Hobbiton-Waikato-IanBrodie-800x600.jpg" alt="" width="300px">
+                <p>Stuff about the listing</p>
+            </div>
+        </div>
+    </div>
+    </div>
+    <br/>
+    <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-center">
+            <li class="page-item disabled">
+                <a class="page-link" href="#" tabindex="-1">Previous</a>
+            </li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item">
+              <a class="page-link" href="#">Next</a>
+            </li>
+        </ul>
+    </nav>
+</div>
 
 
 
