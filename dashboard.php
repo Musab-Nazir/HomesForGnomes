dc1cd4f3a572582100bd03661923be6ee676b464
Musab Nazir <musabnazir@gmail.com>
2018-12-09 21:25:37 -0500

    Dashboard page show listing made by agent
    you dashboard now shows listings you made

--- dashboard.php
+++ dashboard.php
@@ -2,43 +2,80 @@
 /*
 Name:         	Ramandeep Rathor
 Name:           Musab Nizar
-Name:			      Kevin Astilla
-Name:			      Nathan Morris
+Name:			Kevin Astilla
+Name:			Nathan Morris
 Description:  	Dashboard File For Homes For Gnomes
 Date:         	28 September 2018
 */
-  $title = "WEBD2201 - Web Development - Fundamentals";
-  $date = "12 April 2018";
-  $file = "template.php";
-  $banner = "Dashboard";
-  $description = "This page will be a log in page for the website and a new functions.php file will contain some shared functions for use throughout the website.";
 
 require "header.php";
 // If the session was never set with a user id
-if($_SESSION['userType'] != a)
+$output = '';
+$conn = db_connect();
+if($_SESSION['userType'] != "a")
 {
     $_SESSION['RedirectError'] = "You were not logged in as an Agent<br/>";
     header("Location:login.php");
 }
+if (isset($_GET["page"])) {
+    $page  = $_GET["page"];
+}
+else {
+    $page=1;
+ }
+
+
 ?>
   <!-- start of main page content -->
-
-  <div class="container">
+  <div class="container" style="margin-top: 2em;">
       <div class="row">
-            <div class="col"></div>
-            <div class="col-6">
-                <br/>
-                <h2>Dashboard</h2>
-                <p>Welcome back <?php echo $_SESSION['userID']; ?> you last logged in on <?php echo $_SESSION['last_access']; ?></p>
-            </div>
-            <div class="col"></div>
-        </div>
+              <h2>Dashboard</h2>
+
+      </div>
+      <div class="row">
+          <p>Welcome back <?php echo $_SESSION['userID']; ?> you last logged in on <?php echo $_SESSION['last_access']; ?></p>
+      </div>
+      <h4>Your Listings</h4>
+      <?php
+          echo '<div class="row">';
+          $sql = "SELECT listing_id FROM listings WHERE user_id = '".$_SESSION['userID']."'";
+          $result = pg_query($conn, $sql);
+          $listings = pg_fetch_all($result);
+          for($index = 0; $index < pg_num_rows($result); $index++)
+          {
+              $sql = "SELECT * FROM listings WHERE listing_id = '".$listings[$index]['listing_id']."'";
+              $listing_result = pg_query($conn, $sql);
+              echo '<div class="col-md-4"style="margin-top:1em"><div class="card"><div class="card-body">';
+              $arrayRow = pg_fetch_assoc($listing_result);
+              echo (build_listing_card($arrayRow));
+              echo '</div></div></div>';
+          }
+      ?>
+          </div>
+          <br/>
+          <div class="row justify-content-md-center">
+          <?php
+          $total_pages = ceil(count($listings) / IMAGE_LIMIT);
+          $pagLink = "<div class='pagination'>";
+          for ($i=1; $i<=$total_pages; $i++) {
+                       if($i == $page)
+                       {
+                          $pagLink .= "<a class='active' href='dashboard.php?page=".$i."'>".$i."</a>";
+                       }
+                       else {
+                           $pagLink .= "<a href='dashboard.php?page=".$i."'>".$i."</a>";
+                       }
+          };
+          echo $pagLink . "</div>";
+           ?>
+       </div>
+       <br/>
   </div>
 
-  <!-- end of main page content -->
+    <!-- end of main page content -->
 
-<!-- Footer Start -->
-<?php
-  include 'footer.php'
-?>
-<!-- Footer End -->
+  <!-- Footer Start -->
+  <?php
+    include 'footer.php'
+  ?>
+  <!-- Footer End -->
