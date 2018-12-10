969581e6386147f3145bf5d8e60173cadfe00f92
Unknown <ramandeep.rathor97@gmail.com>
2018-12-10 16:13:51 -0500

    Password Request WORKING :D

--- password-request.php
+++ password-request.php
@@ -2,8 +2,8 @@
 /*
 Name:         	Ramandeep Rathor
 Name:           Musab Nazir
-Name:			Kevin Astilla
-Name:		    Nathan Morris
+Name:		       	Kevin Astilla
+Name:		        Nathan Morris
 Description:  	Request Password For Homes For Gnomes
 Date:         	07 December 2018
 */
@@ -13,63 +13,59 @@ require("header.php");
 $error = "";
 $output = "";
 $conn = "";
-$login = "";
+$userID = "";
 // $userMessage = "";
 
 if(isPost()){
   $_SESSION['RedirectError'] = "";
 
-  $login = trim($_POST["user_id"]);
+  $userID = trim($_POST["user_id"]);
 
   //if user didn't enter anything in field
-  if(!isset($login) || $login == ""){
+  if(!isset($userID) || $userID == ""){
   //means the user did not enter anything
   $error .= "<br/>You did not enter a User ID."; //set error
-  $login = "";
+  $userID = "";
   }
   //else if login is too short
-  else if (strlen($login) < MINIMUM_ID_LENGTH) {
-      $error .= "<br/>The username must be at least 5 characters, ".$login." is not long enough."; //set error
-      $login = "";
+  else if (strlen($userID) < MINIMUM_ID_LENGTH) {
+      $error .= "<br/>The username must be at least 5 characters, ".$userID." is not long enough."; //set error
+      $userID = "";
   }
   //else if login is too long
   else if (strlen($login) >= MAXIMUM_ID_LENGTH) {
-      $error .= "<br/>The username must be less than 20 characters, ".$login." is too long."; //set error
-      $login = "";
+      $error .= "<br/>The username must be less than 20 characters, ".$userID." is too long."; //set error
+      $userID = "";
+  }
+  if (!(userExists($userID))){  //Check if user does not exist in database
+    $error .= "<br/>The username entered does NOT exist."; //set error
+    $userID = "";
   }
 
   if($error == ""){  //if error is an empty string, meaning there are no errors with the input
-      $conn = db_connect();
-      //Check if user exists in database
-      if (userExists($login)){
-          $_SESSION['userID'] = $login;
+    //   $conn = db_connect();
 
-          $emailAddress = pg_fetch_assoc(userEmail($userId));
-          $tempPassword = RandomString(); //Generates new password
+    $temp_password = generateRandomString(); //Generates new password
+    $new_password = hash("md5",$temp_password); //Hashes the new password
+    $sql = "UPDATE users SET password = '".$new_password."' WHERE user_id = '".$userID."'";
+    $update = pg_query(db_connect(), $sql);
+    $output .= "A temporary password has been emailed to you. <br /> Please check your email and sign in once you are ready.<br/>";
 
-        //  $userMessage = $emailAddress . "\n" . $tempPassword;
-      }
+    //$output .= "<br />MADE IT TO OUTPUT<br />";
+    $output .= "<br /> TESTING PURPOSES: ".$temp_password."<br />";
+    $output .= "<br /> (HASHED) TESTING PURPOSES: ".$new_password."<br />";
+
   }
   else {
       $error .= "<br/>User NOT found";
       $_SESSION['RedirectError'] = "User does NOT exist.<br/>";
   }
-    // $to = $email;
-    // $subject = 'Password Request';
-    // $message = "Hello ".$_SESSION['userID']." your new temporary password is: ".echo $randstring.".\nPlease make sure to change your password to something you can remember.";
-    // $headers = 'From: help@homesforgnomes.com' . "\r\n" . phpversion();
-    //
-    // mail($to, $subject, $message, $headers); //send email
+    $to = $email;
+    $subject = 'Password Request';
+    $message = "Hello ".$_SESSION['userID']." your new temporary password is: ".echo $randstring.".\nPlease make sure to change your password to something you can remember.";
+    $headers = 'From: help@homesforgnomes.com' . "\r\n" . phpversion();
 
-    //update database with new password so user can log in
-    // $sql = "UPDATE users SET password = ". echo $randstring ." WHERE user_id = '".$_SESSION['userID']."'";
-
-   //Display message that password was changed successfully
-
-   //Provide link to login
-    //redirect user to login page
-    //header( "Location:login.php" );
-    //ob_flush();
+    mail($to, $subject, $message, $headers); //send email
 // }
 // else //user does NOT exist
 // {
@@ -85,6 +81,7 @@ if(isPost()){
         <div class="col"></div>
         <div class="col-6">
             <?php echo $error;
+            echo $output;
             if(isset($_SESSION['RedirectError']))
             {
                 echo $_SESSION['RedirectError'];
